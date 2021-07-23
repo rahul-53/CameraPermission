176613f9e7eef7550684369a97c7abd37392410f
y-rahul <42606824+y-rahul@users.noreply.github.com>
2021-07-24 00:24:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/camerapermission/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.camerapermission;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mBtnPermission;
+    private static final int REQ_CODE =0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBtnPermission=findViewById(R.id.tvBtnPermission);
+        mBtnPermission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] permissions = {Manifest.permission.CAMERA};
+                ActivityCompat.requestPermissions(MainActivity.this,permissions,REQ_CODE);
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull  String[] permissions, @NonNull  int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+            showToast("Camera Permission Granted");
+        }
+        else
+            showToast("Camera Permission denied");
+    }
+    public void showToast(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+}
